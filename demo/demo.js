6407c1d86e14a030e76bcf8c8f063772132fc2df
Matt Gaunt <matt@gauntface.co.uk>
2017-04-04 15:13:40 -0700

    Extremely hacky / basic templating

--- demo/demo.js
+++ demo/demo.js
@@ -8,7 +8,37 @@ class DemoController {
   }
 
   addDisplayItem() {
-    console.log('TODO: Add Display Item.');
+    const templateString = `<div class="mdl-grid mdl-grid--no-spacing">
+      <div class="mdl-cell mdl-cell--6-col">
+        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+          <input class="mdl-textfield__input" type="text" id="summary-text" value="">
+          <label class="mdl-textfield__label" for="summary-text">Label</label>
+        </div>
+      </div>
+      <div class="mdl-cell mdl-cell--4-col">
+        <div class="mdl-textfield mdl-js-textfield  mdl-textfield--floating-label">
+          <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="summary-amount" value="">
+          <label class="mdl-textfield__label" for="summary-amount">Amount</label>
+          <span class="mdl-textfield__error">The value must be a number.</span>
+        </div>
+      </div>
+      <div class="mdl-cell mdl-cell--2-col">
+        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+          <input class="mdl-textfield__input" type="text" id="summary-currency" value="USD">
+          <label class="mdl-textfield__label" for="summary-currency">Currency</label>
+        </div>
+      </div>
+    </div>`;
+    const documentHack = document.createElement('div');
+    documentHack.innerHTML = templateString;
+
+    const newDisplayItem = documentHack.firstChild;
+    const itemsContainer = document.querySelector('.display-items-container');
+
+    // TODO: Upgrade elements.
+    // componentHandler.upgradeElement(newDisplayItem);
+
+    itemsContainer.appendChild(newDisplayItem);
   }
 
   addShippingOpt() {
