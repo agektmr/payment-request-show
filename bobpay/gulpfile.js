fe3df1fe119756bcaca350de6bd534035092c131
Mathieu Perreault <mathieu.perreault@gmail.com>
2017-06-06 21:33:32 -0400

    Static page for bobpay

---
+++ bobpay/gulpfile.js
@@ -0,0 +1,62 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var browserSync = require('browser-sync').create();
+var header = require('gulp-header');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+
+
+// Compile LESS files from /less into /css
+gulp.task('less', function() {
+    return gulp.src('public/less/new-age.less')
+        .pipe(less())
+        .pipe(gulp.dest('public/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Minify compiled CSS
+gulp.task('minify-css', ['less'], function() {
+    return gulp.src('public/css/new-age.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('public/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Minify JS
+gulp.task('minify-js', function() {
+    return gulp.src('public/js/new-age.js')
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('public/js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Run everything
+gulp.task('default', ['less', 'minify-css', 'minify-js']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: ''
+        },
+    })
+})
+
+// Dev task with browserSync
+gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() {
+    gulp.watch('public/less/*.less', ['less']);
+    gulp.watch('public/css/*.css', ['minify-css']);
+    gulp.watch('public/js/*.js', ['minify-js']);
+    // Reloads the browser whenever HTML or JS files change
+    gulp.watch('public/*.html', browserSync.reload);
+    gulp.watch('public/js/**/*.js', browserSync.reload);
+});
